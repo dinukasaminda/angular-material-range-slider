4628811e908e1b97c2cad8e27491decbd9976d4b
dinuka saminda <dinukasaminda@gmail.com>
2019-03-11 08:43:54 +0530

    main lib folder updated

--- slidef/typings/slidef.d.ts
+++ slidef/typings/slidef.d.ts
@@ -5,7 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import { Directionality } from "@angular/cdk/bidi";
+import { Directionality } from '@angular/cdk/bidi';
 import {
   ChangeDetectorRef,
   ElementRef,
@@ -13,10 +13,10 @@ import {
   OnDestroy,
   OnInit,
   Renderer2
-} from "@angular/core";
-import { ControlValueAccessor } from "@angular/forms";
-import { CanColor, CanDisable, HammerInput } from "@angular/material/core";
-import { FocusMonitor } from "@angular/cdk/a11y";
+} from '@angular/core';
+import { ControlValueAccessor } from '@angular/forms';
+import { CanColor, CanDisable, HammerInput } from '@angular/material/core';
+import { FocusMonitor } from '@angular/cdk/a11y';
 /**
  * Provider Expression that allows mat-slidef to register as a ControlValueAccessor.
  * This allows it to support [(ngModel)] and [formControl].
@@ -75,12 +75,12 @@ export declare class MatSlidef extends _MatSlidefMixinBase
    * How often to show ticks. Relative to the step so that a tick always appears on a step.
    * Ex: Tick interval of 4 with a step of 3 will draw a tick every 4 steps (every 12 values).
    */
-  tickInterval: "auto" | number;
+  tickInterval: 'auto' | number;
   private _tickInterval;
   /** @deprecated */
-  _tickIntervalDeprecated: number | "auto";
+  _tickIntervalDeprecated: number | 'auto';
   /** Value of the slidef. */
-  value: number | null;
+  value: any | null;
   private _value;
 
   value2: number | null;
